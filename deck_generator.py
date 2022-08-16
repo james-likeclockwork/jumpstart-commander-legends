a31846c201afd4899ccd914bc7dde3e9ec8de318
James Barnes <james@likeclockwork.org>
2022-08-16 17:34:47 +0200

    Basic Land Generation

--- deck_generator.py
+++ deck_generator.py
@@ -25,8 +25,8 @@ with open('halfdecks.json') as jsonFile:
         jsonData = data['cards']
 
         # Find Commander Options
-        for card in jsonData:
-            commanders.add(card['categories'][0])
+        for entry in jsonData:
+            commanders.add(entry['categories'][0])
         commanders.remove('Common Cards')
         commanders.remove('Commander')
 
@@ -41,13 +41,76 @@ with open('halfdecks.json') as jsonFile:
                 selectedCommanders.append(random.sample(commanders, 1)[0])
         else:
             selectedCommanders = random.sample(commanders, 2)
-        
-        print(selectedCommanders)
 
+        halfdeckSize = {selectedCommanders[0]: 0, selectedCommanders[1]: 0}
         selectedCommanders.append('Common Cards')
         # Build Decklist
         with open('decklist.txt', 'w') as decklist:
             for card in jsonData:
-                if card['categories'][0] in selectedCommanders:
-                    cardOutput="%s %s %s"%(card['quantity'],card['card']['oracleCard']['name'], "#"+card['categories'][0])
+                cardCategory = card['categories'][0]
+                if cardCategory in selectedCommanders:
+                    # Determine Color Identity
+                    if card['card']['oracleCard']['name'].startswith(selectedCommanders[0]):
+                        print(card['card']['oracleCard']['name'], end=' & ')
+                        firstColour = card['card']['oracleCard']['colorIdentity'][0]
+                    elif card['card']['oracleCard']['name'].startswith(selectedCommanders[1]):
+                        print(card['card']['oracleCard']['name'])
+                        secondColour = card['card']['oracleCard']['colorIdentity'][0]
+
+                    # Halfdeck Size Increment
+                    if cardCategory != 'Common Cards':
+                        halfdeckSize[cardCategory] +=1
+
+                    # Write Card to Decklist
+                    cardOutput="%s %s %s"%(card['quantity'],card['card']['oracleCard']['name'], "#"+cardCategory)
                     decklist.write(cardOutput+'\n')
+
+            # Add Basic Lands
+            if firstColour != secondColour:
+                firstTotal =  17 - (halfdeckSize[selectedCommanders[0]] - 30)
+                if firstColour == 'White':
+                    firstLand = 'Plains'
+                elif firstColour == 'Blue':
+                    firstLand = 'Island'
+                elif firstColour == 'Black':
+                    firstLand = 'Swamp'
+                elif firstColour == 'Red':
+                    firstLand = 'Mountain'
+                elif firstColour == 'Green':
+                    firstLand = 'Forest'
+
+                landOutput="%s %s"%(firstTotal, firstLand)
+                decklist.write(landOutput+'\n')
+                
+                secondTotal = 17 - (halfdeckSize[selectedCommanders[1]] - 30)
+                if secondColour == 'White':
+                    secondLand = 'Plains'
+                elif secondColour == 'Blue':
+                    secondLand = 'Island'
+                elif secondColour == 'Black':
+                    secondLand = 'Swamp'
+                elif secondColour == 'Red':
+                    secondLand = 'Mountain'
+                elif secondColour == 'Green':
+                    secondLand = 'Forest'
+                
+                landOutput="%s %s"%(secondTotal, secondLand)
+                decklist.write(landOutput)
+            else:
+                combinedTotal = 34 - (halfdeckSize[selectedCommanders[0]] + halfdeckSize[selectedCommanders[1]] - 60)
+                if firstColour == 'White':
+                    basicLand = 'Plains'
+                elif firstColour == 'Blue':
+                    basicLand = 'Island'
+                elif firstColour == 'Black':
+                    basicLand = 'Swamp'
+                elif firstColour == 'Red':
+                    basicLand = 'Mountain'
+                elif firstColour == 'Green':
+                    basicLand = 'Forest'
+                
+                landOutput="%s %s"%(combinedTotal, basicLand)
+                decklist.write(landOutput)
+
+
+        
